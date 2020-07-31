5f727bf4b1d6adc11c5b128633007a6b0b3f78d9
femina123-design <62275491+femina123-design@users.noreply.github.com>
2020-07-31 10:24:25 +0530

    Add files via upload

---
+++ precache-manifest.895eafee9ee397df23394071452f8f50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df4a9109e568ff86dacdbd7cc739620",
+    "url": "/SampleReactApplication/index.html"
+  },
+  {
+    "revision": "eea561ccb4e418044526",
+    "url": "/SampleReactApplication/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5f85bec20dd9bf0fda83",
+    "url": "/SampleReactApplication/static/js/2.f26c9595.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SampleReactApplication/static/js/2.f26c9595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eea561ccb4e418044526",
+    "url": "/SampleReactApplication/static/js/main.d6ab8240.chunk.js"
+  },
+  {
+    "revision": "a27ffaeabb52fd222609",
+    "url": "/SampleReactApplication/static/js/runtime-main.2f006a6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SampleReactApplication/static/media/logo.5d5d9eef.svg"
+  }
+]);
